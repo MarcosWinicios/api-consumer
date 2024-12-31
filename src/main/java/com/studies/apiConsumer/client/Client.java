a990ec7bd2b379dc86356aa02a245dde8c59417b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-12-30 23:32:45 -0300

    feat(http-client): Add default utility methods to the client interface

--- src/main/java/com/studies/apiConsumer/client/Client.java
+++ src/main/java/com/studies/apiConsumer/client/Client.java
@@ -1,12 +1,27 @@
 package com.studies.apiConsumer.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studies.apiConsumer.request.ListResponseData;
 import com.studies.apiConsumer.request.ObjectResponseData;
 import com.studies.apiConsumer.request.RequestData;
 
+import java.io.IOException;
+
 public interface Client {
 
-    public ObjectResponseData get(RequestData request);
+    default ObjectMapper getObjectMapper(){
+        return new ObjectMapper();
+    }
+
+    default void printTargetUrl(String httpMethod, String url){
+        System.out.println("\nTarget API: " + httpMethod + " " + url + "\n");
+    }
+
+    default void printExternalServiceResponse(String response){
+        System.out.println("Response: \n" + response + "\n");
+    }
+
+    public ObjectResponseData get(RequestData requestData) throws IOException;
 
     public ListResponseData getAll(RequestData requestData);
 
